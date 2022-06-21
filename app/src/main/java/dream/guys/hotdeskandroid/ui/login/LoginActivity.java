93c339c3c484977a6d14e51f10803cc709c24be4
Dreamguys <murali.e@dreamguys.co.in>
2022-06-21 19:09:24 +0530

    Splash and Login Commit @Murali

---
+++ app/src/main/java/dream/guys/hotdeskandroid/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package dream.guys.hotdeskandroid.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dream.guys.hotdeskandroid.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/dream/guys/hotdeskandroid/ui/login/SignInActivity.java
@@ -0,0 +1,39 @@
+package dream.guys.hotdeskandroid.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dream.guys.hotdeskandroid.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnSignIn)
+    Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        ButterKnife.bind(this);
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(SignInActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+
+    }
+}
