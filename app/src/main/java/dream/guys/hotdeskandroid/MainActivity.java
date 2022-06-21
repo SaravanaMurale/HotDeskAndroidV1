7723ec60eb9800c44187d538bce327180f77fbaf
Balasubramaniyan Muthamil Selvam <balasubramaniyan.m@dreamguys.co.in>
2022-06-21 09:18:16 +0530

    dashboard and fragment setups @Bala

--- app/src/main/java/dream/guys/hotdeskandroid/MainActivity.java
+++ app/src/main/java/dream/guys/hotdeskandroid/MainActivity.java
@@ -1,14 +1,37 @@
 package dream.guys.hotdeskandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import dream.guys.hotdeskandroid.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        Menu menu = navView.getMenu();
+        menu.findItem(R.id.navigation_home).setIcon(R.drawable.hotdesk_nav_logo);
+        navView.setItemIconTintList(null);
+
+
     }
 }
