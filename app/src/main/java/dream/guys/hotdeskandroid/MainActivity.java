bd96d3bce1fead00e854e9a837a69ea211b445bc
Balasubramaniyan Muthamil Selvam <balasubramaniyan.m@dreamguys.co.in>
2022-06-22 11:41:20 +0530

    Night Mode Change @Bala

--- app/src/main/java/dream/guys/hotdeskandroid/MainActivity.java
+++ app/src/main/java/dream/guys/hotdeskandroid/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.annotation.SuppressLint;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Build;
@@ -26,19 +28,17 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     BottomNavigationView navView;
     NavController navController;
-    Menu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
 
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         uiInit();
-/*
-        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        nightModeConfig();
+/*      navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
@@ -54,36 +54,33 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-*/
+        */
+    }
+
+    private void nightModeConfig() {
+        int nightModeFlags = getApplicationContext().getResources().getConfiguration().uiMode &
+                Configuration.UI_MODE_NIGHT_MASK;
+        Menu menu = navView.getMenu();
+
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+
+                break;
+            case Configuration.UI_MODE_NIGHT_NO:
+                break;
+            case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                break;
+        }
+
     }
 
     private void uiInit() {
         navView = findViewById(R.id.nav_view);
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        menu = navView.getMenu();
-//        menu.findItem(R.id.navigation_home).setIcon(R.drawable.bottom_nav_selector);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            menu.findItem(R.id.navigation_home).setIconTintList(null);
-        }
         navView.setItemIconTintList(null);
 
-        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_home:
-                        System.out.println("home click balaa");
-                    case R.id.navigation_book:
-                        System.out.println("book click balaa");
-                    case R.id.navigation_locate:
-                        System.out.println("locate click balaa");
-
-                }
-                return true;
-            }
-        });
 
     }
 }
