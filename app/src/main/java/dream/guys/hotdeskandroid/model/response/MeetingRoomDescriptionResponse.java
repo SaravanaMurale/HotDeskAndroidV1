1d0ea635eb62c8aa8e406f5d7e9af51e1eec092f
Dreamguys <murali.e@dreamguys.co.in>
2022-07-28 11:12:27 +0530

    Meeting Room BookingCompleted Commit @Murali

---
+++ app/src/main/java/dream/guys/hotdeskandroid/model/response/MeetingRoomDescriptionResponse.java
@@ -0,0 +1,21 @@
+package dream.guys.hotdeskandroid.model.response;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MeetingRoomDescriptionResponse {
+
+    @SerializedName("description")
+    String description;
+
+    public MeetingRoomDescriptionResponse(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
