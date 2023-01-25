069f24ff0b2632fb3635a7742e09ed980bcc4cb1
Balasubramaniyan Muthamil Selvam <balasubramaniyan.m@dreamguystech.com>
2023-01-25 18:41:00 +0530

    french update from murali @bala

--- app/src/main/java/com/brickendon/hdplus/adapter/BookingListToEditAdapter.java
+++ app/src/main/java/com/brickendon/hdplus/adapter/BookingListToEditAdapter.java
@@ -107,13 +107,21 @@ public class BookingListToEditAdapter extends RecyclerView.Adapter<BookingListTo
         switch (bookingsListToEdit.get(position).getUsageTypeId()){
             case 7:
                 holder.editCode.setText("Request for desk");
-                for (int i=0;i<teamDeskAvailabilities.size();i++){
-                    if (bookingsListToEdit.get(position).getRequestedTeamDeskId()
-                            == teamDeskAvailabilities.get(i).getTeamDeskId()){
+                try {
+                    if (teamDeskAvailabilities!=null){
+                        for (int i=0;i<teamDeskAvailabilities.size();i++){
+                            if (Utils.checkStringParms(bookingsListToEdit.get(position).getRequestedTeamDeskId())
+                                    == Utils.checkStringParms(teamDeskAvailabilities.get(i).getTeamDeskId())){
 //                        System.out.println("teamdeskIF"+teamDeskAvailabilities.get(i).getDeskCode());
-                        holder.editCode.setText("Request for desk");
+                                holder.editCode.setText("Request for desk");
+                            }
+                        }
                     }
+
+                } catch (Exception e){
+
                 }
+
                 makeVisible(holder,position);
                 break;
             case 9:
